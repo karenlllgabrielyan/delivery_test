904294ada14d9492eee89bd4f9905f12c8011f5b
karenlllgabrielyan <karen.gabrielyan93@gmail.com>
2020-07-07 21:38:06 +0400

    Starting MVVM

--- app/src/main/java/com/example/delivery/fragments/StopsFragmentRepository.java
+++ app/src/main/java/com/example/delivery/fragments/StopsFragmentRepository.java
@@ -1,9 +1,9 @@
 package com.example.delivery.fragments;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.example.delivery.dao.MarkDao;
 import com.example.delivery.databases.MarkDatabase;
@@ -23,25 +23,91 @@ public class StopsFragmentRepository {
         markDao = database.markDao();
         marks = markDao.getAllMarks();
     }
-
+//####################################################################################### DB METHODTS
+    //------------------------------------------------------- INSERT
     public void insert(Mark mark) {
-
+        new InsertMarkAsyncTask(markDao).execute(mark);
     }
-
+    //------------------------------------------------------- UPDATE
     public void update(Mark mark) {
-
+        new UpdateMarkAsyncTask(markDao).execute(mark);
     }
-
+    //------------------------------------------------------- DELETE
     public void delete(Mark mark){
 
     }
-
+    //------------------------------------------------------- DELETE ALL
     public void deleteAllMarks(){
 
     }
 
     public LiveData<ArrayList<Mark>> getMarks() {
         return marks;
+    }
+//############################################################################################## DB METHOD ASYNC TASKS
+
+    //----------------------------------------------------------------------------------- INSERT
+    private static class InsertMarkAsyncTask extends AsyncTask<Mark, Void, Void>{
+
+        private MarkDao markDao;
+
+        private InsertMarkAsyncTask(MarkDao markDao){
+            this.markDao = markDao;
+        }
+
+        @Override
+        protected Void doInBackground(Mark... marks) {
+            markDao.insert(marks[0]);
+            return null;
+        }
+    }
+
+    //----------------------------------------------------------------------------------- UPDATE
+    private static class UpdateMarkAsyncTask extends AsyncTask<Mark, Void, Void>{
+
+        private MarkDao markDao;
+
+        private UpdateMarkAsyncTask(MarkDao markDao){
+            this.markDao = markDao;
+        }
+
+        @Override
+        protected Void doInBackground(Mark... marks) {
+            markDao.update(marks[0]);
+            return null;
+        }
+    }
+
+    //----------------------------------------------------------------------------------- DELETE
+    private static class DeleteMarkAsyncTask extends AsyncTask<Mark, Void, Void>{
+
+        private MarkDao markDao;
+
+        private DeleteMarkAsyncTask(MarkDao markDao){
+            this.markDao = markDao;
+        }
+
+        @Override
+        protected Void doInBackground(Mark... marks) {
+            markDao.delete(marks[0]);
+            return null;
+        }
+    }
+
+    //----------------------------------------------------------------------------------- DELETE ALL MARKS
+    private static class DeleteAllMarksAsyncTask extends AsyncTask<Void, Void, Void>{
+
+        private MarkDao markDao;
+
+        private DeleteAllMarksAsyncTask(MarkDao markDao){
+            this.markDao = markDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            markDao.deleteAllMarks();
+            return null;
+        }
     }
 
     //    public void initMarksList(MutableLiveData<ArrayList<Mark>> markListLiveData){
